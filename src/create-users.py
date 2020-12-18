1377db0ee41ce7f530974ce99810dabb72ad65eb
marcelooliveira <marcelo.r.oliveira@ebradi.com.br>
2020-12-18 16:14:06 -0300

    mass mfa removal

---
+++ src/create-users.py
@@ -0,0 +1,104 @@
+# pip install pipenv
+# pipenv install requests
+from mysecrets import ONELOGIN_CLIENT_ID, ONELOGIN_CLIENT_SECRET
+
+import requests
+import json
+
+# 1. Complete the following steps using the OneLogin API.
+api_domain = 'https://api.us.onelogin.com'
+r = requests.post(api_domain + '/auth/oauth2/v2/token',
+  auth=(ONELOGIN_CLIENT_ID, ONELOGIN_CLIENT_SECRET),
+  json={
+    "grant_type": "client_credentials"
+  }
+)
+response = r.json()
+
+access_token = response['access_token']
+headers = headers = {'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'}
+
+
+
+
+
+
+# 2. Get the role named "Test". 
+response = requests.get(api_domain + '/api/2/roles?name=Test', headers=headers)
+json_data = json.loads(response.content)
+test_role_id = 0
+user_ids = []
+for role in json_data:
+    if role["name"] == "Test":
+        user_ids = role["users"]
+        test_role_id = role["id"]
+        break
+
+# 3. Delete test users
+requests.delete(api_domain + '/api/2/roles/' + str(test_role_id) + '/users', headers=headers)
+for user_id in user_ids:
+    response = requests.delete(api_domain + '/api/2/users/' + str(user_id), headers=headers)
+
+# 4. Delete test role
+requests.delete(api_domain + '/api/2/roles/' + str(test_role_id), headers=headers)
+
+
+
+
+
+
+
+
+
+
+
+# 2. Create a role named "Test". 
+role_data = {
+    "name":"Test"
+}
+
+response = requests.post(api_domain + '/api/2/roles', headers=headers, data=json.dumps(role_data))
+json_data = json.loads(response.content)
+test_role_id = json_data['id']
+
+
+# 3. Get OneLoginMFAGroup group
+response = requests.get(api_domain + '/api/1/groups', headers=headers)
+json_data = json.loads(response.content)
+group_id = json_data['data'][0]['id']
+
+# 4. Create a couple of users. 
+user_data = {
+    "email": "user1@myemail.com",
+    "firstname": "user",
+    "lastname": "one",
+    "username": "user1",
+    "password": "useruser1",
+    "password_confirmation": "useruser1",
+    "role_ids":[
+            str(test_role_id)
+         ],
+    "group_id": str(group_id)
+}
+
+response = requests.post(api_domain + '/api/2/users', headers=headers, data=json.dumps(user_data))
+json_data = json.loads(response.content)
+user1_id = json_data['id']
+response = requests.put(api_domain + '/api/2/users', headers=headers, data=json.dumps(user_data))
+
+user_data = {
+    "email": "user2@myemail.com",
+    "firstname": "user",
+    "lastname": "two",
+    "username": "user2",
+    "password": "useruser2",
+    "password_confirmation": "useruser2",
+    "role_ids":[
+            str(test_role_id)
+         ],
+    "group_id": str(group_id)
+}
+
+response = requests.post(api_domain + '/api/2/users', headers=headers, data=json.dumps(user_data))
+json_data = json.loads(response.content)
+user2_id = json_data['id']
--- src/mass-remove-mfa.py
+++ src/mass-remove-mfa.py
@@ -5,102 +5,56 @@ from mysecrets import ONELOGIN_CLIENT_ID, ONELOGIN_CLIENT_SECRET
 import requests
 import json
 
-# 1. Complete the following steps using the OneLogin API.
+
+
+def get_onelogin_headers():
+    r = requests.post(api_domain + '/auth/oauth2/v2/token',
+    auth=(ONELOGIN_CLIENT_ID, ONELOGIN_CLIENT_SECRET),
+    json={
+        "grant_type": "client_credentials"
+    }
+    )
+    response = r.json()
+
+    access_token = response['access_token']
+    return {'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'}
+
+def get_user_ids():
+    response = requests.get(api_domain + '/api/2/roles?name=Test', headers=headers)
+    json_data = json.loads(response.content)
+    user_ids = []
+    for role in json_data:
+        if role["name"] == "Test":
+            user_ids = role["users"]
+            break
+    return user_ids
+
+def remove_factor_for_each_user(user_ids, factor_name):
+    for user_id in user_ids:
+        device_id = get_enrolled_mfa_device(user_id, factor_name)
+        if device_id:
+            remove_mfa_device(user_id, device_id)
+
+def get_enrolled_mfa_device(user_id, factor_name):
+    response = requests.get(api_domain + '/api/1/users/' + str(user_id) + '/otp_devices', headers=headers)
+    json_data = json.loads(response.content)
+    device_id = 0
+    for device in json_data["data"]["otp_devices"]:
+        if device["auth_factor_name"] == factor_name:
+            device_id = device["id"]
+            break
+    return device_id
+
+def remove_mfa_device(user_id, device_id):
+    requests.delete(api_domain + '/api/1/users/' + str(user_id) + '/otp_devices/' + str(device_id), headers=headers)
+
+
 api_domain = 'https://api.us.onelogin.com'
-r = requests.post(api_domain + '/auth/oauth2/v2/token',
-  auth=(ONELOGIN_CLIENT_ID, ONELOGIN_CLIENT_SECRET),
-  json={
-    "grant_type": "client_credentials"
-  }
-)
-response = r.json()
+factor_name = "OneLogin Protect"
 
-access_token = response['access_token']
-headers = headers = {'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'}
-
-# 2. Create a role named "Test". 
-role_data = {
-    "name":"Test"
-}
-
-response = requests.post(api_domain + '/api/2/roles', headers=headers, data=json.dumps(role_data))
-json_data = json.loads(response.content)
-test_role_id = json_data['id']
-
-
-# 3. Get OneLoginMFAGroup group
-response = requests.get(api_domain + '/api/1/groups', headers=headers)
-json_data = json.loads(response.content)
-group_id = json_data['data'][0]['id']
-
-# 4. Create a couple of users. 
-user_data = {
-    "email": "amelie.gagnon@myemail.com",
-    "firstname": "Amélie",
-    "lastname": "Gagnon",
-    "username": "Amélie Gagnon",
-    "role_ids":[
-            str(test_role_id)
-         ],
-    "group_id": str(group_id),
-    "phone": "+5511998215929"
-}
-
-response = requests.post(api_domain + '/api/2/users', headers=headers, data=json.dumps(user_data))
-json_data = json.loads(response.content)
-user1_id = json_data['id']
-response = requests.put(api_domain + '/api/2/users', headers=headers, data=json.dumps(user_data))
-
-user_data = {
-    "email": "thomas.tremblay@myemail.com",
-    "firstname": "Thomas",
-    "lastname": "Tremblay",
-    "username": "Thomas Tremblay",
-    "role_ids":[
-            str(test_role_id)
-         ],
-    "group_id": str(group_id)
-}
-
-response = requests.post(api_domain + '/api/2/users', headers=headers, data=json.dumps(user_data))
-json_data = json.loads(response.content)
-user2_id = json_data['id']
-
-# 4. Get available factors
-response = requests.get(api_domain + '/api/2/mfa/users/' + str(user1_id) + '/factors', headers=headers)
-json_data = json.loads(response.content)
-factor_id = 0
-for factor in json_data:
-    if factor["name"] == "OneLogin SMS":
-        factor_id = factor["factor_id"]
-        break
-
-# 5. Enroll a Factor
-
-# mfa_data = {
-#     "factor_id": str(factor_id),
-#     "display_name": "OneLogin SMS"
-# }
-
-# response = requests.post(api_domain + '/api/2/users/' + str(user1_id) + '/registrations', headers=headers, data=json.dumps(mfa_data))
-
-mfa_data = {
-    "factor_id": str(factor_id),
-    "display_name": "OneLogin SMS",
-    "number": "+5511998215929"
-}
-
-response = requests.post(api_domain + '/api/1/users/' + str(user1_id) + '/otp_devices', headers=headers, data=json.dumps(mfa_data))
-
-# THE ABOVE POST REQUEST WILL PRODUCE A HTTP 401 UNAUTHORIZED
-
-
-# 6. Get Enrolled Factors
-response = requests.get(api_domain + '/api/2/mfa/users/' + str(user1_id) + '/devices', headers=headers)
-json_data = json.loads(response.content)
-device_id = 0
-for device in json_data:
-    if factor["auth_factor_name"] == "OneLogin SMS":
-        device_id = factor["device_id"]
-        break
-
+# 1. Complete the following steps using the OneLogin API.
+headers = get_onelogin_headers()
+# 2. Get the role named "Test". 
+user_ids = get_user_ids()
+# 3. Remove factor for each user
+remove_factor_for_each_user(user_ids, factor_name)
